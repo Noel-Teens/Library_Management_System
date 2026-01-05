18de2fd90fb7e8b3652aeb24ad8f8fd768d91fd7
Noel-Teens <teeniejoshua@gmail.com>
2026-01-05 18:59:32 +0530

    Quotes changed in app.listen

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port ${PORT}");
+  console.log('Server running on port ${PORT}');
   console.log("Available endpoints:");
   console.log("  POST   /books                 - Create new book");
   console.log("  GET    /books                 - Get all books");
